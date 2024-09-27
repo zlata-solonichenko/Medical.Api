2ab30e671d8aed3e91cb04080ba304b3ac35ca85
chibichaan <xokkko0@mail.ru>
2024-09-27 18:35:29 +0300

    заполнение контроллеров

--- Medical.Api/Controllers/DoctorController.cs
+++ Medical.Api/Controllers/DoctorController.cs
@@ -14,11 +14,64 @@ public class DoctorController : ControllerBase
         _doctorRepository = doctorRepository;
     }
 
-    public async Task<IActionResult> Index()
+    [HttpGet("all")] // GET: api/doctor
+    public async Task<IActionResult> GetAllDoctors()
     {
         var doctors = await _doctorRepository.GetAllAsync();
         return Ok(doctors);
     }
     
-    //далее остальные методы
+    [HttpGet("{id}")] // GET: api/doctor/{id}
+    public async Task<ActionResult<Doctor>> GetDoctorById(int id)
+    {
+        var doctor = await _doctorRepository.GetByIdAsync(id);
+        if (doctor == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(doctor);
+    }
+
+    [HttpPost("create")] // POST: api/doctor
+    public async Task<IActionResult> AddDoctor([FromBody] Doctor doctor)
+    {
+        if (doctor == null)
+        {
+            return BadRequest();
+        }
+        await _doctorRepository.AddAsync(doctor);
+        return CreatedAtAction(nameof(GetDoctorById), new { id = doctor.Id }, doctor);
+    }
+
+    [HttpPut("delete {id}")] // DELETE: api/doctor/{id}
+    public async Task<IActionResult> DeleteDoctor(int id)
+    {
+        var doctor = await _doctorRepository.GetByIdAsync(id);
+        if (doctor == null)
+        {
+            return NotFound();
+        }
+
+        await _doctorRepository.DeleteAsync(id);
+        return Ok();
+    }
+
+    [HttpPut("update {id}")] // PUT: api/doctor/{id}
+    public async Task<IActionResult> UpdateDoctor(int id, [FromBody] Doctor doctor)
+    {
+        if (doctor == null || doctor.Id != id)
+        {
+            return BadRequest();
+        }
+
+        var existingDoctor = await _doctorRepository.GetByIdAsync(id);
+        if (existingDoctor == null)
+        {
+            return NotFound();
+        }
+
+        await _doctorRepository.UpdateAsync(doctor);
+        return Ok(); //NoContent
+    }
 }
--- Medical.Api/Controllers/PatientController.cs
+++ Medical.Api/Controllers/PatientController.cs
@@ -14,11 +14,64 @@ public class PatientController : ControllerBase
         _patientRepository = patientRepository;
     }
 
-    public async Task<IActionResult> Index()
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllPatients()
     {
         var patients = await _patientRepository.GetAllAsync();
         return Ok(patients);
     }
     
-    //далее остальные методы
+    [HttpGet("{id}")] // GET: api/patient/{id}
+    public async Task<ActionResult<Patient>> GetPatientById(int id)
+    {
+        var patient = await _patientRepository.GetByIdAsync(id);
+        if (patient == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(patient);
+    }
+
+    [HttpPost("create")] // POST: api/patient
+    public async Task<IActionResult> AddPatient([FromBody] Patient patient)
+    {
+        if (patient == null)
+        {
+            return BadRequest();
+        }
+        await _patientRepository.AddAsync(patient);
+        return CreatedAtAction(nameof(GetPatientById), new { id = patient.Id }, patient);
+    }
+
+    [HttpPut("delete {id}")] // DELETE: api/patient/{id}
+    public async Task<IActionResult> DeletePatient(int id)
+    {
+        var patient = await _patientRepository.GetByIdAsync(id);
+        if (patient == null)
+        {
+            return NotFound();
+        }
+
+        await _patientRepository.DeleteAsync(id);
+        return Ok();
+    }
+
+    [HttpPut("update {id}")] // PUT: api/patient/{id}
+    public async Task<IActionResult> UpdatePatient(int id, [FromBody] Patient patient)
+    {
+        if (patient == null || patient.Id != id)
+        {
+            return BadRequest();
+        }
+
+        var existingPatient = await _patientRepository.GetByIdAsync(id);
+        if (existingPatient == null)
+        {
+            return NotFound();
+        }
+
+        await _patientRepository.UpdateAsync(patient);
+        return Ok(); //NoContent
+    }
 }
